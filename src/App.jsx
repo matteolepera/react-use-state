6f22719c5701f6c89db6b8d5c62e81670583b297
matteolpera <matteo.lepera02@gmail.com>
2025-11-28 16:07:56 +0100

    feat: gestita la card dinamicamente

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,24 @@ import { languages } from "./data/languages"
 
 
 function App() {
-  const [variant, setVariant] = useState(null);
+  const [variant, setVariant] = useState(false);
   return (
     <div className="container">
       <h1>Learn Web development</h1>
       {languages.map((language) =>
-        <button key={language.id} onClick={() => setVariant(language.id)} className={`btn ${variant === language.id ? "btn-warning" : "btn-primary"} m-3`}>{language.title}</button>
+        <button key={language.id} onClick={() => setVariant(language)} className={`btn ${variant && variant.id === language.id ? "btn-warning" : "btn-primary"} m-3`}>{language.title}</button>
       )}
-      <div class="card">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        </div>
-      </div>
+      {
+        variant && (
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">{variant.title}</h5>
+              <p className="card-text">{variant.description}</p>
+            </div>
+          </div>
+        )
+      }
+
     </div>
   )
 }
