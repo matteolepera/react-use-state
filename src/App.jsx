d12d77f247519d9fc6649884cd50ff6a86292f7a
matteolpera <matteo.lepera02@gmail.com>
2025-11-28 14:52:41 +0100

    feat: bottoni aggiunti tramite map alla pagina.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { languages } from "./data/languages"
+
 
 function App() {
 
 
   return (
-    <>
-      <h1>use state</h1>
-    </>
+    <div className="container">
+      <h1>Learn Web development</h1>
+      {languages.map((language) =>
+        <button className="btn btn-primary mt-3 mx-2">{language.title}</button>
+      )}
+
+    </div>
   )
 }
 
