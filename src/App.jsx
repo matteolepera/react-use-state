f5954fb55301ef3b97a475b730d1ecaccc80fb50
matteolpera <matteo.lepera02@gmail.com>
2025-11-28 15:25:32 +0100

    feat: aggiunta la variante al click del bottone

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import { languages } from "./data/languages"
 
 
 function App() {
-
-
+  const [variant, setVariant] = useState(null);
   return (
     <div className="container">
       <h1>Learn Web development</h1>
       {languages.map((language) =>
-        <button className="btn btn-primary mt-3 mx-2">{language.title}</button>
+        <>
+          <button key={language.id} onClick={() => setVariant(language.id)} className={`btn ${variant === language.id ? "btn-warning" : "btn-primary"} m-3`}>{language.title}</button>
+          {/* <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            </div>
+          </div> */}
+        </>
       )}
-
     </div>
   )
 }
